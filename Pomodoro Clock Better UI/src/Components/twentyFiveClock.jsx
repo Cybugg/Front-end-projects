d2b4e36edbeb967f3b3b02f5edd2a58b9bf27033
Cybug Timehin <93569544+Cybugg@users.noreply.github.com>
2022-06-05 20:31:08 +0100

    Update twentyFiveClock.jsx

--- Pomodoro Clock Better UI/src/Components/twentyFiveClock.jsx
+++ Pomodoro Clock Better UI/src/Components/twentyFiveClock.jsx
@@ -1,7 +1,6 @@
 import React from "react"
 import Countdown from "react-countdown"
 import {CountdownCircleTimer,useCountdown} from "react-countdown-circle-timer"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Button from '@mui/material/Button';
 import SpeedDial from '@mui/material/SpeedDial';
 import SpeedDialIcon from '@mui/material/SpeedDialIcon';
